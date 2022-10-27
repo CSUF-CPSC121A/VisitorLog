9ca9d6f2d8648c85c3428e6d3c09fbd1a7dcc3a8
pinventado <>
2022-10-27 00:20:04 +0000

    Use iterator to traverse visitor cwids

--- visitorlog.h
+++ visitorlog.h
@@ -9,23 +9,25 @@ class VisitorLog {
     visitor_cwids_.push_back(cwid);
   }
   int TotalVisitors() { return visitor_cwids_.size(); }
-  bool Visited(int cwid) {
-    std::vector<int>::iterator it
-        = std::find(visitor_cwids_.begin(), 
-                    visitor_cwids_.end(), cwid);
-    return it != visitor_cwids_.end();
-  }
   void DisplayLog() {
     int counter = 1;
-    for (int cwid : visitor_cwids_) {
-      std::cout << counter << ". " << cwid << "\n";
+    for (std::vector<int>::iterator 
+         it = visitor_cwids_.begin(); 
+         it != visitor_cwids_.end(); 
+         it++) {
+      std::cout << counter << ". " << *it << "\n";
       counter++;
     }
   }
   void Remove(int index) {
     visitor_cwids_.erase(visitor_cwids_.begin() + index);
   }
-
+  bool Visited(int cwid) {
+    std::vector<int>::iterator it
+        = std::find(visitor_cwids_.begin(), 
+                    visitor_cwids_.end(), cwid);
+    return it != visitor_cwids_.end();
+  }
  private:
   std::vector<int> visitor_cwids_;   
 };
